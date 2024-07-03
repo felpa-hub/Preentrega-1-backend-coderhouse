5ba48775016b0c900cae5588c1c66634353277f9
felpa-hub <68981238+felpa-hub@users.noreply.github.com>
2024-07-03 10:53:11 -0300

    Create cart.js

---
+++ models/cart.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    products: [{
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+        quantity: { type: Number, required: true }
+    }]
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
